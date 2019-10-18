3838b7f8b327b47f73366369013128d7e145a62c
T D <shashank.t.d@intel.com>
2019-10-18 14:21:35 +0530

    Added extra checks for checking changes and origin[D[D[D[D[D[Dset remote origin

--- git_upload.sh
+++ git_upload.sh
@@ -2,14 +2,32 @@
 
 echo 'Taking this as your git repo :'$PWD
 
-echo 'Add files to upload : '
-read files
-git add $files
+echo 'Checking for changes :'
+repo_changed=$(git diff)
 
-echo 'Add Commit message'
-read msg
-git commit -m "$msg"
+if [ "$repo_changed" == 0 ]
+then
+	echo "First change something to upload!"
+	.
+else
+	echo 'Add files to upload : '
+	read files
+	git add $files
 
-echo 'branch name to upload :'
-read branch
-git push origin $branch
+	echo 'Add Commit message'
+	read msg
+	git commit -m "$msg"
+	
+	check_origin=$(git config --get remote.origin.url)
+	if [ "$check_origin" == 0 ]
+	then
+		echo "Origin is not set. So setting it"
+		echo "Enter Repo URL : "
+		read repo_url
+		git remote add origin $repo_url
+		.
+	fi
+	echo 'branch name to upload :'
+	read branch
+	git push origin $branch
+fi
